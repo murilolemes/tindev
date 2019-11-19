58206ce761341cb648f3e72e518be9ba560a17ba
murilolemes <murilo.lemes.mhl@gmail.com>
2019-11-19 16:57:18 -0300

    altecacoes no backend, autenticacao banco de dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 
 const server = express();
 
-mongoose.connect('mongodb+srv://murilo:henrique@cluster0-bbvil.mongodb.net/omnistack8?retryWrites=true&w=majority' , {
+mongoose.connect('mongodb+srv://murilo:henrique@omnistack-bbvil.mongodb.net/omnistack8?retryWrites=true&w=majority' , {
     useNewUrlParser: true
 });
 
